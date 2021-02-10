7a65aef50b4e9447d4bf1b51f70a9d78df858afc
Bryan Summersett <bryan@summerse.tt>
2021-02-09 20:47:02 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,6 +8,7 @@ let package = Package(
     products: [
         .library(
             name: "GiphyUISDK",
+            type: .dynamic,
             targets: ["GiphyUISDKWrapper"]),
     ],
     dependencies: [
